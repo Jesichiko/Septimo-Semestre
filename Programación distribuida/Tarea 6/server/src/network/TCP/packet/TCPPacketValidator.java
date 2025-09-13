c32fdd0e42df68316908d9f874d36b1fffa44490
jesichi/linux <jesichiko@gmail.com>
2025-09-12 23:42:33 -0600

    Tarea servidor DNS

---
+++ Programación distribuida/Tarea 6/server/src/network/TCP/packet/TCPPacketValidator.java
@@ -0,0 +1,32 @@
+package packet;
+
+public class TCPPacketValidator {
+  private final static Long MAX_SEQUENCE = (long)Math.pow(2, 32) - 1;
+
+  public static int calculateChecksum(TCPPacket packet) {
+    // Serializamos el TCPPacket
+    byte[] data = TCPPacketSerializer.serialize(packet.withChecksum(0));
+
+    // Hacemos una checksum sumando cada bit
+    int checksum = 0;
+    for (byte b : data) {
+      checksum += (b & 0xFF);
+      checksum = (checksum & 0xFFFF) + (checksum >> 16);
+    }
+
+    // Retornamos su complemento a 16 bits
+    return (~checksum) & 0xFFFF;
+  }
+
+  public static boolean validateChecksum(TCPPacket packet) {
+    // Calculamos el checksum del packet y verificamos
+    int calculateChecksumd = calculateChecksum(packet.withChecksum(0));
+    return calculateChecksumd == packet.getChecksum();
+  }
+  
+  public static boolean validateSequenceNumbers(long number, long ackNumber){
+    // Hacemos wraparound al number
+    long expected = (number + 1) % (MAX_SEQUENCE + 1);
+    return expected == ackNumber;
+  }
+}
