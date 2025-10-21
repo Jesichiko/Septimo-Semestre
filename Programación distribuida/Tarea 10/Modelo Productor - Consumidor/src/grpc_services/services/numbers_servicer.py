44b438cae3079947dc8ddce243da4d42bac45240
jesichi/linux <jesichiko@gmail.com>
2025-10-21 12:48:43 -0600

    Tarea 10

--- Programación distribuida/Tarea 10/Modelo Productor - Consumidor/src/grpc_services/services/numbers_servicer.py
+++ Programación distribuida/Tarea 10/Modelo Productor - Consumidor/src/grpc_services/services/numbers_servicer.py
@@ -1,11 +1,13 @@
 from grpc import StatusCode
 from grpc_services.protos import numbers_service_pb2, numbers_service_pb2_grpc
 from grpc_services.utils.generate_random_numbers import Generate_Numbers
+from grpc_services.utils.user_stats import UserStats
 
 
 class NumbersServicer(numbers_service_pb2_grpc.NumbersServiceServicer):
-    def __init__(self, number_generator: Generate_Numbers):
+    def __init__(self, number_generator: Generate_Numbers, user_stats: UserStats):
         self.generator = number_generator
+        self.stats = user_stats
         pass
 
     def getNumbers(self, request, context):
@@ -17,7 +19,31 @@ class NumbersServicer(numbers_service_pb2_grpc.NumbersServiceServicer):
             print("[SERVICIO NUMBERS: ERROR] Error al crear vector de numeros")
             context.set_code(StatusCode.ABORTED)
             context.set_details(f"Error al crear vector de numeros: {e}")
+            return numbers_service_pb2.NumbersResponse()
 
         return numbers_service_pb2.NumbersResponse(
             num1=numbers[0], num2=numbers[1], num3=numbers[2]
         )
+
+    def receiveResult(self, request, context):
+        if not request.user_addr or request.user_addr.strip() == "":
+            print("[ERROR RECEIVE RESULT] Direccion de usuario invalida")
+            context.set_code(StatusCode.INVALID_ARGUMENT)
+            context.set_details("Campo IP invalido en mensaje")
+            return numbers_service_pb2.ResultResponse(response=False)
+
+        if not request.result:
+            print("[ERROR RECEIVE RESULT] Resultado no proporcionado")
+            context.set_code(StatusCode.INVALID_ARGUMENT)
+            context.set_details("Resultado no proporcionado en mensaje")
+            return numbers_service_pb2.ResultResponse(response=False)
+
+        print(
+            f"[SERVER: RECEIVE RESULT] Resultado {request.result} recibido de {
+                request.user_addr
+            }"
+        )
+
+        self.stats.addResult(request.user_addr, request.result)
+        print("[SERVER: RESULTADO REGISTRADO] Resultado almacenado exitosamente")
+        return numbers_service_pb2.ResultResponse(response=True)
---
+++ Programación distribuida/Tarea 10/Modelo Productor - Consumidor/src/grpc_services/utils/user_stats.py
@@ -0,0 +1,7 @@
+# Estadisticas de los usuarios que enviarons resultados
+# Con la addr del usuario se cuentan sus "participaciones" (+1 cada result)
+# y se impriminen los usuarios y cuanto han trabajado
+# y ademas se suman todos los resultados que se recibieron (suma de todos)
+
+class UserStats():
+    pass
