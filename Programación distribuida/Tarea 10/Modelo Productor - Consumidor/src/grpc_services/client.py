b5ca0019d6f6c8e44fc1ed8767d2c56a56686488
jesichi/linux <jesichiko@gmail.com>
2025-10-21 21:51:42 -0600

    Tarea 10

--- Programación distribuida/Tarea 10/Modelo Productor - Consumidor/src/grpc_services/client.py
+++ Programación distribuida/Tarea 10/Modelo Productor - Consumidor/src/grpc_services/client.py
@@ -46,8 +46,8 @@ def send_result(server_address: str, client_addr: str, result: float) -> bool:
 def main():
     # Argumentos: python client.py <server_ip> <server_port> <client_ip> <client_port>
     server_ip = sys.argv[1] if len(sys.argv) > 1 else "0.0.0.0"
-    client_ip = sys.argv[3] if len(sys.argv) > 3 else "0.0.0.0"
-    port = int(sys.argv[4]) if len(sys.argv) > 4 else 8080
+    client_ip = sys.argv[2] if len(sys.argv) > 3 else "0.0.0.0"
+    port = int(sys.argv[3]) if len(sys.argv) > 4 else 8080
 
     server_address = f"{server_ip}:{port}"
     client_addr = f"{client_ip}:{port}"
