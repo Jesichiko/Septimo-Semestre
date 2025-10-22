05c73e05e59d3c2f80d404e2b9384fea694e514f
jesichi/linux <jesichiko@gmail.com>
2025-10-21 21:40:37 -0600

    Tarea 10

--- Programación distribuida/Tarea 10/Modelo Productor - Consumidor/src/grpc_services/client.py
+++ Programación distribuida/Tarea 10/Modelo Productor - Consumidor/src/grpc_services/client.py
@@ -46,12 +46,11 @@ def send_result(server_address: str, client_addr: str, result: float) -> bool:
 def main():
     # Argumentos: python client.py <server_ip> <server_port> <client_ip> <client_port>
     server_ip = sys.argv[1] if len(sys.argv) > 1 else "0.0.0.0"
-    server_port = int(sys.argv[2]) if len(sys.argv) > 2 else 8080
     client_ip = sys.argv[3] if len(sys.argv) > 3 else "0.0.0.0"
-    client_port = int(sys.argv[4]) if len(sys.argv) > 4 else 8080
+    port = int(sys.argv[4]) if len(sys.argv) > 4 else 8080
 
-    server_address = f"{server_ip}:{server_port}"
-    client_addr = f"{client_ip}:{client_port}"
+    server_address = f"{server_ip}:{port}"
+    client_addr = f"{client_ip}:{port}"
 
     print(f"[CLIENTE: INICIO] {client_addr} conectando a servidor {server_address}")
     try:
